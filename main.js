eccee6fb62d2a7e11bf58d6cce05c92e0a976ed7
Nyaann <88116907+Nyaannn@users.noreply.github.com>
2023-03-07 19:38:55 -0700

    better

--- main.js
+++ main.js
@@ -1,23 +1,2 @@
 function login(t){"2023"==t?(key=prompt("Astro Key:"),key=="walterwhite".toLowerCase()?this.location="methodsel.html":this.close()):this.close()}
-function redirect(method){
-    if(method==1){
-        alert('Sorry no astro proxy exists yet!')
-    }
-    if(method==2){
-        window.open("https://www.cloudflare.com/apps/0VJ1mCYqPTh4/install", "_blank", "width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,");
-        this.close();
-    }
-    if(method==3){
-        window.open("https://r.codewars.cf", "_blank", "width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,");
-        this.close();
-    }
-    if(method==4){
-        window.open("./misc/gamer.html", "_blank", "width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,");
-        this.close();
-    }
-    if(method==5){
-        window.open("./misc/Minecraft.html", "_blank", "width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,");
-        this.close();
-    }
-    
-}
+function redirect(o){1==o&&alert("Sorry no astro proxy exists yet!"),2==o&&(window.open("https://www.cloudflare.com/apps/0VJ1mCYqPTh4/install","_blank","width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,"),this.close()),3==o&&(window.open("https://r.codewars.cf","_blank","width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,"),this.close()),4==o&&(window.open("./misc/gamer.html","_blank","width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,"),this.close()),5==o&&(window.open("./misc/Minecraft.html","_blank","width=900,height=900,status=yes, toolbar=no, menubar=no, location=no,"),this.close())}
